e8b73963e710c52d874188642aaf09ae670e272c
Keyur Vaghasiya <107289889+Keyur0016@users.noreply.github.com>
2023-05-26 14:08:25 +0530

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,7 +1,2 @@
-console.log("Run chat") ; 
+console.log("Run chat123") ; 
 console.log(window.location.href)
-
-window.__be = window.__be || {};
-window.__be.id = "61f28451fdd7c5000728b4f9"; 
-
-console.log(window.__be.id) ; 
